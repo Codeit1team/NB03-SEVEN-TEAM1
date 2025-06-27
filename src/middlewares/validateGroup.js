3ea1081b56a185f4e50baade12318c050c8aea66
qkrwotjd1731 <58104546+qkrwotjd1731@users.noreply.github.com>
2025-06-27 10:48:29 +0900

    validateGroup.js 1차완성
    validateRecord.js의 error 처리 그대로 복사

--- src/middlewares/validateGroup.js
+++ src/middlewares/validateGroup.js
@@ -1,11 +1,33 @@
-import { array, integer, min, object, size, string } from 'superstruct';
+import * as struct from 'superstruct'
 
-export const createGroup = object({
-  name: size(string(), 1, 20),
-  description: size(string(), 1, 300),
-  photoUrl: string(),
-  goalRep: integer(),
-  discordWebhookUrl: string(),
-  discordInviteUrl: string(),
-  tags: array(size(string(), 1, 10)),
+export const createGroup = struct.object({
+  name: struct.size(struct.string(), 1, 20),
+  description: struct.optional(struct.size(struct.string(), 0, 500)),
+  goalRep: struct.refine(struct.integer(), 'PositiveInt', (value) => value >= 0),
+  tags: struct.array(struct.size(struct.string(), 1, 20)),
+  ownerId: struct.integer(),
 });
+
+export const patchGroup = struct.partial(createGroup);
+
+export const validateCreateRecord = (req, res, next) => {
+  const [error] = struct.validate(req.body, createRecord);
+
+  if (error) {
+    const field = error.path[0];
+    const message = field ? `${field} 해당 데이터가 유효하지 않습니다` : '데이터가 잘못되었습니다';
+    return res.status(400).json({ message });
+  }
+  next();
+};
+
+export const validatePatchRecord = (req, res, next) => {
+  const [error] = validate(req.body, patchRecord);
+
+  if (error) {
+    const field = error.path[0];
+    const message = field ? `${field} 해당 데이터가 유효하지 않습니다` : '데이터가 잘못되었습니다';
+    return res.status(400).json({ message });
+  }
+  next();
+};
