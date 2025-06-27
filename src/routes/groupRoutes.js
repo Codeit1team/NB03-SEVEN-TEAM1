f4062a9e13b347516bc275a64c9955e2364f2453
qkrwotjd1731 <58104546+qkrwotjd1731@users.noreply.github.com>
2025-06-27 16:55:14 +0900

    groupRoutes.js 오타 수정

--- src/routes/groupRoutes.js
+++ src/routes/groupRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
-import { validateCreateGroup, validatePatchGroup } from '#middlewares/validateRecord';
-import { uploadImages } from '#middlewares/upload';
+import { validateCreateGroup, validatePatchGroup } from '#middlewares/validateGroup.js';
+import { uploadImages } from '#middlewares/upload.js';
 import GroupController from '#controllers/groupController.js';
 
 const GroupRouter = Router();
