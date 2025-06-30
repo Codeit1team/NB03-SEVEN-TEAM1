6b7b59f71081d68c5b360a94483442143a9b5756
luciakim22 <luciakim22@naver.com>
2025-06-30 16:41:14 +0900

    tags init

--- src/services/GroupService.js
+++ src/services/GroupService.js
@@ -6,16 +6,25 @@ const createTag = async (req, res, next) => {
   try {
     //태그 유효성 검사 넣어야하나?
     const { id: groupId } = req.params;
-    const { name = [] } = req.body;
+    let { tagName } = req.body;
 
-    const tagData = name.map((tagName) => ({
-      name: tagName,
-      groupId: Number(groupId),
-    }))
+    if (typeof tagName === 'string') {
+      tagName = [tagName]
+    }
 
-    const tag = await prisma.tag.create({
-      data: tagData
-    })
+    const createdTags = []
+
+    for (const name of tagName) {
+      const tag = await prisma.tag.create({
+        data: {
+          name,
+          groups: { connect: { id: Number(groupId) } }
+        }
+      })
+      createdTags.push(tag)
+    }
+
+    return res.status(201).json(createdTags)
   } catch (error) {
     if (error?.name === 'StructError') {
       return res.status(400).end()
