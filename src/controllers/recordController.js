54a49a194746e69c9d909ada974848407a98a994
choYoungWook <wwwe2410@naver.com>
2025-06-30 16:06:47 +0900

    update registration

--- src/controllers/recordController.js
+++ src/controllers/recordController.js
@@ -1,9 +1,21 @@
+import { PrismaClient } from '@prisma/client';
 import RecordService from "#services/RecordService.js";
+import sendDiscordWebhook from "#utils/sendDiscordWebhook.js";
+
+const prisma = new PrismaClient();
 
 const createRecord = async (req, res, next) => {
   try {
     req.body.photos = req.files.map(file => `http://localhost:3000/uploads/${file.filename}`);
     const record = await RecordService.createRecord(req.body);
+    const groupId = req.params.id;
+    const group = await prisma.group.findUnique({
+      where: { id: groupId },
+      select: { webhookUrl: true },
+    });
+    if (group.webhookUrl) {
+      await sendDiscordWebhook(group.webhookUrl, `${req.body.authorNickname}운동기록 등록이 완료되었습니다`);
+    }
     return res.status(201).json(record);
   } catch (error) {
     error.status = 400;
