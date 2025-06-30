4e2a41bf5c8dbd495c4d430b2d0f4aa60c34bfeb
luciakim22 <luciakim22@naver.com>
2025-06-30 09:10:57 +0900

    주석처리 회수

--- src/routes/recordRoutes.js
+++ src/routes/recordRoutes.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-// import { validateCreateRecord, validatePatchRecord } from '#middlewares/validateRecord.js';
+import { validateCreateRecord, validatePatchRecord } from '#middlewares/validateRecord.js';
 import { uploadImages } from '#middlewares/upload.js';
 import RecordController from '#controllers/recordController.js';
 
 const RecordRouter = Router();
 
 RecordRouter.route('/:id')
-  // .post(uploadImages(),validateCreateRecord,RecordController.createRecord)
+  .post(uploadImages(),validateCreateRecord,RecordController.createRecord)
   .get(RecordController.getRecordDetail)
 
 export default RecordRouter
