be97a94b4f717200a616b5ff5609babc7d3496b9
choYoungWook <wwwe2410@naver.com>
2025-07-02 15:30:19 +0900

    주석수정

--- src/middlewares/validateRecord.js
+++ src/middlewares/validateRecord.js
@@ -1,21 +1,21 @@
 import * as struct from 'superstruct'
 import deleteUploadedFiles from '#utils/deleteUploadedFiles.js';
 
-// const createRecord = struct.object({
-//   exerciseType: struct.enums(['RUN', 'BIKE', 'SWIM']),
-//   description: struct.optional(struct.size(struct.string(), 0, 500)),
-//   time: struct.refine(struct.integer(), 'timeLimit', (value) => {
-//     return value > 0 && value <= (3600 * 1000 * 10)
-//   }),
-//   distance: struct.refine(struct.number(), 'distanceLimit', (value) => {
-//     return value > 0 && value <= 1000
-//   }),
-//   authorNickname: struct.size(struct.string(), 1, 20),
-//   authorPassword: struct.size(struct.string(), 4, 20)
-// });
+const createRecord = struct.object({
+  exerciseType: struct.enums(['RUN', 'BIKE', 'SWIM']),
+  description: struct.optional(struct.size(struct.string(), 0, 500)),
+  time: struct.refine(struct.integer(), 'timeLimit', (value) => {
+    return value > 0 && value <= (3600 * 1000 * 10)
+  }),
+  distance: struct.refine(struct.number(), 'distanceLimit', (value) => {
+    return value > 0 && value <= 1000
+  }),
+  authorNickname: struct.size(struct.string(), 1, 20),
+  authorPassword: struct.size(struct.string(), 4, 20)
+});
 
 //  postman 테스트 코드 포스트맨은 int,float 값 보낼수없이 전부 string이라 테스트코드변경
-
+/*
 const timeStruct = struct.refine(
   struct.coerce(struct.integer(), struct.string(), (value) => parseInt(value, 10)),
   'timeLimit',
@@ -36,35 +36,36 @@ const createRecord = struct.object({
   authorNickname: struct.size(struct.string(), 1, 20),
   authorPassword: struct.size(struct.string(), 4, 20),
 });
+*/
 
+const validateCreateRecord = async (req, res, next) => {
+  const [error] = struct.validate(req.body, createRecord);
 
-// const validateCreateRecord = async (req, res, next) => {
-//   const [error] = struct.validate(req.body, createRecord);
-
-//   if (error) {
-//     await deleteUploadedFiles(req.files.phoyos);
-//     const field = error.path?.[0];
-//     const message = field ? `${field} 해당 데이터가 유효하지 않습니다` : '데이터가 잘못되었습니다';
-//     return res.status(400).json({ message });
-//   }
-//   next();
-// };
-
-// postman 테스트 코드
-
-export const validateCreateRecord = async (req, res, next) => {
-  try {
-    const validated = struct.create(req.body, createRecord);
-    req.body = validated;
-    next();
-  } catch (error) {
-    console.log(error)
-    if (req.files.photos) await deleteUploadedFiles(req.files.photos);
+  if (error) {
+    await deleteUploadedFiles(req.files.phoyos);
     const field = error.path?.[0];
-    const message = field ? `${field} 해당 데이터가 유효하지 않습니다`: '데이터가 잘못되었습니다';
+    const message = field ? `${field} 해당 데이터가 유효하지 않습니다` : '데이터가 잘못되었습니다';
     return res.status(400).json({ message });
   }
-}; 
+  next();
+};
+
+// postman 테스트 코드
+/*
+// export const validateCreateRecord = async (req, res, next) => {
+//   try {
+//     const validated = struct.create(req.body, createRecord);
+//     req.body = validated;
+//     next();
+//   } catch (error) {
+//     console.log(error)
+//     if (req.files.photos) await deleteUploadedFiles(req.files.photos);
+//     const field = error.path?.[0];
+//     const message = field ? `${field} 해당 데이터가 유효하지 않습니다`: '데이터가 잘못되었습니다';
+//     return res.status(400).json({ message });
+//   }
+// };
+*/
 
 const validateGetRecords = (req, res, next) => {
   const { page = 1, limit = 10, order = 'createdAt', orderBy = 'desc', duration = 'weekly'} = req.query;
