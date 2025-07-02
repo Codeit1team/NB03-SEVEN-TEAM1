11376e4631f8b0c39414e588db846e4723eb376f
luciakim22 <luciakim22@naver.com>
2025-07-02 09:31:40 +0900

    tagService

---
+++ src/services/tagService.js
@@ -0,0 +1,69 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+const createTag = async (req, res, next) => {
+  try {
+    //태그 유효성 검사 넣어야하나?
+    const { id: groupId } = req.params;
+    let { tagName } = req.body;
+
+    if (typeof tagName === 'string') {
+      tagName = [tagName]
+    }
+
+    const createdTags = []
+
+    for (const name of tagName) {
+      const tag = await prisma.tag.create({
+        data: {
+          name,
+          groups: { connect: { id: Number(groupId) } }
+        }
+      })
+      createdTags.push(tag)
+    }
+
+    return res.status(201).json(createdTags)
+  } catch (error) {
+    if (error?.name === 'StructError') {
+      return res.status(400).end()
+    }
+  }
+}
+
+const searchGroupsByTag = async (req, res, next) => {
+  try {
+    const { tag } = req.query
+
+    const groups = await prisma.group.findMany({
+      where: {
+        tags: {
+          some: {
+            name: {
+              equals: tag,
+              mode: 'insensitive'
+            }
+          }
+        }
+      },
+      include: {
+        tags: true,
+        owner: {
+          select: {
+            id: true,
+            nickname: true,
+          }
+        }
+      }
+    })
+
+    res.status(200).json(groups)
+  } catch (error) {
+    if (error?.name === 'StructError') {
+      return res.status(400).end()
+    }
+  }
+}
+
+export default { createTag, searchGroupsByTag }
