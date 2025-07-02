8d01c2a633e9e4ff63ee7677f29c8118d0149b66
qkrwotjd1731 <58104546+qkrwotjd1731@users.noreply.github.com>
2025-07-02 10:38:42 +0900

    prefix/add participantRouter

--- src/controllers/participantController.js
+++ src/controllers/participantController.js
@@ -6,7 +6,7 @@ const createParticipant = async (req, res, next) => {
     return res.status(201).json(participant);
   } catch (error) {
     error.status = 400;
-    error.message = '기록 생성에 실패했습니다. 데이터가 올바른지 확인해주세요.';
+    error.message = '참여자 생성에 실패했습니다. 데이터가 올바른지 확인해주세요.';
     next(error);
   }
 };
--- src/services/ParticipantService.js
+++ src/services/ParticipantService.js
@@ -1,12 +1,31 @@
 import { PrismaClient } from '@prisma/client';
+import { hashPassword } from "#utils/passwordUtil.js";
 
 const prisma = new PrismaClient();
 
 const createParticipant = async (data) => {
-  return await prisma.participant.create({
-    data,
-  })
+  try {
+      const result = await prisma.$transaction(async (tx) => {
+        const hashedPassword = await hashPassword(ownerPassword);
+        const owner = await tx.participant.create({
+          data: {
+            nickname: ownerNickname,
+            password: hashedPassword
+          }
+        });
+      }
+    )
+  } catch (error) {
+    console.error('❌ GroupService 에러:', error);
+    throw new Error(`그룹 생성 실패: ${error.message}`);
+  }
+  // return await prisma.participant.create({
+  //   data,
+  // })
 };
+
+
+
 
 export default { createParticipant };
 
