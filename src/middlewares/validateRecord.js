03383322f82c0bc0d84527c122ff5b20d77b6a9e
choYoungWook <wwwe2410@naver.com>
2025-07-03 13:29:22 +0900

    int로 변환 수정

--- src/middlewares/validateRecord.js
+++ src/middlewares/validateRecord.js
@@ -1,74 +1,41 @@
 import * as struct from 'superstruct'
 import deleteUploadedFiles from '#utils/deleteUploadedFiles.js';
 
-// const createRecord = struct.object({
-//   exerciseType: struct.enums(['RUN', 'BIKE', 'SWIM']),
-//   description: struct.optional(struct.size(struct.string(), 0, 500)),
-//   time: struct.refine(struct.integer(), 'timeLimit', (value) => {
-//     return value > 0 && value <= (3600 * 1000 * 10)
-//   }),
-//   distance: struct.refine(struct.number(), 'distanceLimit', (value) => {
-//     return value > 0 && value <= 1000
-//   }),
-//   authorNickname: struct.size(struct.string(), 1, 20),
-//   authorPassword: struct.size(struct.string(), 4, 20)
-// });
-
-//  postman 테스트 코드 포스트맨은 int,float 값 보낼수없이 전부 string이라 테스트코드변경
-
-const timeStruct = struct.refine(
-  struct.coerce(struct.integer(), struct.string(), (value) => parseInt(value, 10)),
-  'timeLimit',
-  (value) => value > 0 && value <= (3600 * 1000 * 10)
-);
-
-const distanceStruct = struct.refine(
-  struct.coerce(struct.number(), struct.string(), (value) => parseFloat(value)),
-  'distanceLimit',
-  (value) => value > 0 && value <= 1000
-);
-
 const createRecord = struct.object({
   exerciseType: struct.enums(['RUN', 'BIKE', 'SWIM']),
   description: struct.optional(struct.size(struct.string(), 0, 500)),
-  time: timeStruct,
-  distance: distanceStruct,
+  time: struct.refine(struct.integer(), 'timeLimit', (value) => {
+    return value > 0 && value <= (3600 * 1000 * 10)
+  }),
+  distance: struct.refine(struct.number(), 'distanceLimit', (value) => {
+    return value > 0 && value <= 1000
+  }),
   authorNickname: struct.size(struct.string(), 1, 20),
-  authorPassword: struct.size(struct.string(), 4, 20),
+  authorPassword: struct.size(struct.string(), 4, 20)
 });
 
+const validateCreateRecord = async (req, res, next) => {
+  if (req.body.time && typeof req.body.time === 'string'){
+    req.body.time = parseInt(req.body.time)
+  }
 
-// const validateCreateRecord = async (req, res, next) => {
-//   const [error] = struct.validate(req.body, createRecord);
+  if (req.body.distance && typeof req.body.distance === 'string'){
+    req.body.distance = Number(req.body.distance)
+  }
 
-//   if (error) {
-//     await deleteUploadedFiles(req.files.photos);
-//     const field = error.path?.[0];
-//     const message = field ? `${field} 해당 데이터가 유효하지 않습니다` : '데이터가 잘못되었습니다';
-//     return res.status(400).json({ message });
-//   }
-//   next();
-// };
+  const [error] = struct.validate(req.body, createRecord);
 
-// postman 테스트 코드
-
-export const validateCreateRecord = async (req, res, next) => {
-  try {
-    const validated = struct.create(req.body, createRecord);
-    req.body = validated;
-    next();
-  } catch (error) {
-    console.log(error)
-    if (req.files.photos) await deleteUploadedFiles(req.files.photos);
+  if (error) {
+    await deleteUploadedFiles(req.files.photos);
     const field = error.path?.[0];
-    const message = field ? `${field} 해당 데이터가 유효하지 않습니다`: '데이터가 잘못되었습니다';
+    const message = field ? `${field} 해당 데이터가 유효하지 않습니다` : '데이터가 잘못되었습니다';
     return res.status(400).json({ message });
   }
+  next();
 };
 
-
 const validateGetRecords = (req, res, next) => {
-  const { page = 1, limit = 10, order = 'createdAt', orderBy = 'desc', duration = 'weekly'} = req.query;
+  const { page = 1, limit = 10, order = 'createdAt', orderBy = 'desc', duration = 'weekly' } = req.query;
 
   const intPage = parseInt(page, 10);
   const intLimit = parseInt(limit, 10);
@@ -93,7 +60,7 @@ const validateGetRecords = (req, res, next) => {
 
   const allowedDuration = ['weekly', 'monthly'];
   if (!allowedDuration.includes(duration)) {
-    return res.status(400).json({error: 'duration은 weekly, monthly만 가능합니다'});
+    return res.status(400).json({ error: 'duration은 weekly, monthly만 가능합니다' });
   }
 
   next();
