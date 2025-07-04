c2232fb20e80529c1ebd521449f1023042824e7c
luciakim22 <luciakim22@naver.com>
2025-07-04 17:51:06 +0900

    same

--- src/controllers/tagController.js
+++ src/controllers/tagController.js
@@ -1,9 +1,9 @@
 import TagService from '#services/tagService.js'
 
-const getTags = async (req, res, next) => {
+const getTagList = async (req, res, next) => {
   try {
     const { search = '', page = '1', limit = '10', order = 'desc', orderBy = 'createdAt' } = req.query //req.query로 들어오는 값이 모두 문자열이기 때문에 '1'처럼 표시해줘야 함. 이후 pageNum으로 숫자로 변환
-    const { data, total } = await TagService.getTags({search, page: parseInt(page, 10), limit: parseInt(limit, 10), order, orderBy})
+    const { data, total } = await TagService.getTagList({search, page: parseInt(page, 10), limit: parseInt(limit, 10), order, orderBy})
     res.status(200).json({ data, total })
   } catch (error) {
     if (error.message === '태그 리스트를 불러올 수 없습니다.') {
@@ -14,4 +14,4 @@ const getTags = async (req, res, next) => {
   }
 }
 
-export default { getTags }
+export default { getTagList }
--- src/services/tagService.js
+++ src/services/tagService.js
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
 
-const getTags = async ({ search = '', page = 1, limit = 10, order = 'desc', orderBy = 'createdAt' }) => {
+const getTagList = async ({ search = '', page = 1, limit = 10, order = 'desc', orderBy = 'createdAt' }) => {
   if (typeof search !== 'string') {
     throw new Error('태그 리스트를 불러올 수 없습니다.')
   }
@@ -33,4 +33,33 @@ const getTags = async ({ search = '', page = 1, limit = 10, order = 'desc', orde
   return { data: tags, total }
 }
 
-export default { getTags }
+// const getTag = async (req, res, next) => {
+//   try {
+//     const { tagId } = req.query
+
+//     const tag = await prisma.tag.findUnique({
+//       where: {
+//         tags: {
+//           some: {
+//             name: {
+//               equals: { id: tagId },
+//               mode: 'insensitive'
+//             }
+//           }
+//         }
+//       },
+//       select: {
+//         name: true,
+//         createdAt: true,
+//         updatedAt: true,
+//       }
+//     })
+
+//     res.status(200).json(tag)
+//   } catch (error) {
+//     console.error('searchGroupsByTag 오류:', error.message)
+//     return res.status(500).json({ message: '서버 오류가 발생했습니다.' })
+//   }
+// }
+
+export default { getTagList, getTag }
