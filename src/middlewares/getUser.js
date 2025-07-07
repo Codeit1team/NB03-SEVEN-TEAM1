498b78661aba9b476bc48ae8f953629e4a564804
selentia <admin@duna.me>
2025-07-07 18:49:45 +0900

    Check for 'req.files.photos' existence before accessing uploaded photos

--- src/middlewares/getUser.js
+++ src/middlewares/getUser.js
@@ -18,18 +18,18 @@ const getUser = async (req, res, next) => {
     });
 
     if (!user) {
-      if (req.files.photos) await deleteUploadedFiles(req.files.photos);
+      if (req.files && req.files.photos) await deleteUploadedFiles(req.files.photos);
       return res.status(401).json({ message: '그룹에 존재하지않는 참여자 입니다' });
     }
     const isValid = await isPasswordValid(authorPassword, user.password);
     if (!isValid) {
-      if (req.files.photos) await deleteUploadedFiles(req.files.photos);
+      if (req.files && req.files.photos) await deleteUploadedFiles(req.files.photos);
       return res.status(401).json({ message: '비밀번호가 일치하지 않습니다' });
     }
     req.body.authorId = user.id
     next();
   } catch (err) {
-    if (req.files.photos) await deleteUploadedFiles(req.files.photos);
+    if (req.files && req.files.photos) await deleteUploadedFiles(req.files.photos);
     err.status = 500;
     err.message = '서버 통신에 문제가 생겼습니다';
     next(err);
