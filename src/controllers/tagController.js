d40ea33fb7ad284d24be387256bcbe9f502a249c
luciakim22 <luciakim22@naver.com>
2025-07-07 16:54:01 +0900

    오류 질문용 커밋

--- src/controllers/tagController.js
+++ src/controllers/tagController.js
@@ -1,16 +1,13 @@
 import tagService from '#services/tagService.js';
-import TagService from '#services/tagService.js'
-import handleServerError from "#utils/handleServerError.js";
 
 const getTagList = async (req, res, next) => {
   try {
-    const { search = '', page = 1, limit = 10, order = 'desc', orderBy = 'createdAt' } = req.query //req.query로 들어오는 값이 모두 문자열이기 때문에 '1'처럼 표시해줘야 함. 이후 pageNum으로 숫자로 변환
-    const { data, total } = await TagService.getTagList({ search, page: parseInt(page, 10), limit: parseInt(limit, 10), order, orderBy })
+    const { search = '', page = 1, limit = 10, order = 'desc', orderBy = 'createdAt' } = req.query
+    const { data, total } = await tagService.getTagList({ search, page: parseInt(page, 10), limit: parseInt(limit, 10), order, orderBy })
     res.status(200).json({ data, total })
   } catch (error) {
     next(error);
   }
-  next();
 }
 
 const getTag = async (req, res, next) => {
--- src/middlewares/validateTag.js
+++ src/middlewares/validateTag.js
@@ -1,28 +1,28 @@
 const validateTagList = async (req, res, next) => {
-  const { search = '', order = 'desc' } = req.query;
-  
-  if (typeof req.query.search !== 'string') {
-    const error = new Error('태그 리스트를 불러올 수 없습니다.')
-    error.status = 404
-    return next(error)
-  }
+  try {
+    if (typeof req.query.search !== 'string') {
+      const error = new Error('태그 리스트를 불러올 수 없습니다.')
+      error.status = 404
+      return next(error)
+    }
 
-  if (!['asc', 'desc'].includes(order)) {
-    order = 'desc'
+    if (!['asc', 'desc'].includes(req.query.order)) {
+      req.query.order = 'desc'
+    };
+
+    next();
   };
 
-  next();
-};
+  const validateTagSearch = async (req, res, next) => {
+    const { tagId } = req.query
 
-const validateTagSearch = async (req, res, next) => {
-  const { tagId } = req.query
-
-  if (!tagId || isNaN(Number(tagId))) {
-    const error = new Error('태그가 존재하지 않습니다.')
-    error.status = 404
-    return next(error)
-  }
-  next();
+    if (!tagId || isNaN(Number(tagId))) {
+      const error = new Error('태그가 존재하지 않습니다.')
+      error.status = 404
+      return next(error)
+    }
+    next();
+  } catch (err) {}
 }
 
 export default {
