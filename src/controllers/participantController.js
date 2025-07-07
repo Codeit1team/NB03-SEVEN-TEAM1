79df48d9aae53cb7f422326b64cbf1cdc2f704d0
selentia <admin@duna.me>
2025-07-07 15:40:04 +0900

    chore: add missing handleServerError import and temporary groupId param

--- src/controllers/participantController.js
+++ src/controllers/participantController.js
@@ -1,12 +1,15 @@
 import ParticipantService from "#services/ParticipantService.js";
+import handleServerError from "#utils/handleServerError.js";
 
 const createParticipant = async (req, res, next) => {
   try {
+    const groupId = parseInt(req.params.groupId);
+
     const participantData = {
       ...req.body,
       groupId
     };
-    
+
     const participant = await ParticipantService.createParticipant(participantData);
     return res.status(201).json(participant);
   } catch (error) {
@@ -16,16 +19,18 @@ const createParticipant = async (req, res, next) => {
 
 const deleteParticipant = async (req, res, next) => {
   try {
+    const groupId = parseInt(req.params.groupId);
+
     const participantData = {
       ...req.body,
       groupId
     };
-    
+
     await ParticipantService.deleteParticipant(participantData);
     return res.status(204).send();
   } catch (error) {
     next(handleServerError(error, '서버 내부 오류로 그룹 탈퇴에 실패했습니다.'));
   }
-}
+};
 
 export default { createParticipant, deleteParticipant };
