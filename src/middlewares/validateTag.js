d6719a7a1ae0d65a37a87c38f7310a456b72b9c8
luciakim22 <luciakim22@naver.com>
2025-07-07 17:01:05 +0900

    tagList PR

--- src/middlewares/validateTag.js
+++ src/middlewares/validateTag.js
@@ -1,28 +1,36 @@
 const validateTagList = async (req, res, next) => {
   try {
-    if (typeof req.query.search !== 'string') {
+    const {
+      search = '',
+      order = 'desc',
+    } = req.query;
+
+    if (typeof search !== 'string') {
       const error = new Error('태그 리스트를 불러올 수 없습니다.')
       error.status = 404
       return next(error)
     }
 
-    if (!['asc', 'desc'].includes(req.query.order)) {
+    if (!['asc', 'desc'].includes(order)) {
       req.query.order = 'desc'
     };
 
     next();
-  };
+  } catch (error) {
+    error.status = error.status || 400;
+    next(error);
+  }
+}
 
-  const validateTagSearch = async (req, res, next) => {
-    const { tagId } = req.query
+const validateTagSearch = async (req, res, next) => {
+  const { tagId } = req.query
 
-    if (!tagId || isNaN(Number(tagId))) {
-      const error = new Error('태그가 존재하지 않습니다.')
-      error.status = 404
-      return next(error)
-    }
-    next();
-  } catch (err) {}
+  if (!tagId || isNaN(Number(tagId))) {
+    const error = new Error('태그가 존재하지 않습니다.')
+    error.status = 404
+    return next(error)
+  }
+  next();
 }
 
 export default {
