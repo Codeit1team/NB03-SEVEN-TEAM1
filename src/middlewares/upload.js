e3af4823c8fdb9b99320b009667072ac897d6002
selentia <admin@duna.me>
2025-07-09 21:23:16 +0900

    fix: delete upload Type(jpg)

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -55,7 +55,7 @@ export const uploadImages = ({ maxCount = 5 } = {}) => {
   });
 
   const fileFilter = (req, file, cb) => {
-    const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg', 'image/webp'];
+    const allowedTypes = ['image/jpeg', 'image/png', 'image/webp'];
     if (allowedTypes.includes(file.mimetype)) {
       cb(null, true);
     } else {
